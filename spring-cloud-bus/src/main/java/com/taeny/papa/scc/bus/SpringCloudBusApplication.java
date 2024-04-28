81d43b7aff4d67123dd587e7e43f649705b520b9
seongho.2 <seongho.2@hyundai.com>
2024-04-28 14:14:00 +0900

    rabbitmq 기반 기반 설정 동기화

---
+++ spring-cloud-bus/src/main/java/com/taeny/papa/scc/bus/SpringCloudBusApplication.java
@@ -0,0 +1,13 @@
+package com.taeny.papa.scc.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudBusApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-bus/src/test/java/com/taeny/papa/scc/bus/SpringCloudBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taeny.papa.scc.bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudBusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
